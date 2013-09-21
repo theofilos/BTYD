cc0137366ba3955330e5b33740aa2b89dedc23df
theofilos <theofilos13@gmail.com>
2013-09-21 15:50:06 -0700

    fixed BTYD with log exp sum trick

--- pnbd.R
+++ pnbd.R
@@ -19,58 +19,53 @@ pnbd.cbs.LL <- function(params, cal.cbs) {
     return(sum(custs * pnbd.LL(params, x, t.x, T.cal)))
 }
 
-pnbd.LL <- function(params, x, t.x, T.cal) {
-    
-    max.length <- max(length(x), length(t.x), length(T.cal))
-    
-    if (max.length%%length(x)) 
-        warning("Maximum vector length not a multiple of the length of x")
-    if (max.length%%length(t.x)) 
-        warning("Maximum vector length not a multiple of the length of t.x")
-    if (max.length%%length(T.cal)) 
-        warning("Maximum vector length not a multiple of the length of T.cal")
-    
-    dc.check.model.params(c("r", "alpha", "s", "beta"), params, "pnbd.LL")
-    
-    if (any(x < 0) || !is.numeric(x)) 
-        stop("x must be numeric and may not contain negative numbers.")
-    if (any(t.x < 0) || !is.numeric(t.x)) 
-        stop("t.x must be numeric and may not contain negative numbers.")
-    if (any(T.cal < 0) || !is.numeric(T.cal)) 
-        stop("T.cal must be numeric and may not contain negative numbers.")
-    
-    
-    x <- rep(x, length.out = max.length)
-    t.x <- rep(t.x, length.out = max.length)
-    T.cal <- rep(T.cal, length.out = max.length)
-    
-    r <- params[1]
-    alpha <- params[2]
-    s <- params[3]
-    beta <- params[4]
-    
-    maxab <- max(alpha, beta)
-    absab <- abs(alpha - beta)
-    param2 <- s + 1
-    if (alpha < beta) {
-        param2 <- r + x
-    }
-    part1 <- r * log(alpha) + s * log(beta) - lgamma(r) + lgamma(r + x)
-    part2 <- -(r + x) * log(alpha + T.cal) - s * log(beta + T.cal)
-    if (absab == 0) {
-        F1 <- -(r + s + x) * log(maxab + t.x)
-        F2 <- -(r + s + x) * log(maxab + T.cal)
-        ## subLogs defined in dc.R. subLogs(a,b) = log(exp(a) - exp(b))
-        partF <- subLogs(F1, F2)
-    } else {
-        F1 <- hyperg_2F1(r + s + x, param2, r + s + x + 1, absab/(maxab + t.x))/((maxab + 
-            t.x)^(r + s + x))
-        F2 <- hyperg_2F1(r + s + x, param2, r + s + x + 1, absab/(maxab + T.cal))/((maxab + 
-            T.cal)^(r + s + x))
-        partF <- log(F1 - F2)
-    }
-    part3 <- log(s) - log(r + s + x) + partF
-    return(part1 + log(exp(part2) + exp(part3)))
+pnbd.LL= function (params, x, t.x, T.cal) 
+{
+  max.length <- max(length(x), length(t.x), length(T.cal))
+  if (max.length%%length(x)) 
+    warning("Maximum vector length not a multiple of the length of x")
+  if (max.length%%length(t.x)) 
+    warning("Maximum vector length not a multiple of the length of t.x")
+  if (max.length%%length(T.cal)) 
+    warning("Maximum vector length not a multiple of the length of T.cal")
+  dc.check.model.params(c("r", "alpha", "s", "beta"), params, 
+                        "pnbd.LL")
+  if (any(x < 0) || !is.numeric(x)) 
+    stop("x must be numeric and may not contain negative numbers.")
+  if (any(t.x < 0) || !is.numeric(t.x)) 
+    stop("t.x must be numeric and may not contain negative numbers.")
+  if (any(T.cal < 0) || !is.numeric(T.cal)) 
+    stop("T.cal must be numeric and may not contain negative numbers.")
+  x <- rep(x, length.out = max.length)
+  t.x <- rep(t.x, length.out = max.length)
+  T.cal <- rep(T.cal, length.out = max.length)
+  r <- params[1]
+  alpha <- params[2]
+  s <- params[3]
+  beta <- params[4]
+  maxab <- max(alpha, beta)
+  absab <- abs(alpha - beta)
+  param2 <- s + 1
+  if (alpha < beta) {
+    param2 <- r + x
+  }
+  part1 <- r * log(alpha) + s * log(beta) - lgamma(r) + lgamma(r + x)
+  part2 <- -(r + x) * log(alpha + T.cal) - s * log(beta + T.cal)
+  if (absab == 0) {    
+    part2_times_F1_min_F2 <- ( (alpha+T.cal)/(maxab+t.x) )^(r+x) * (beta+T.cal)^s / 
+      ((maxab+t.x)^s) -
+      ( (alpha+T.cal)/(maxab+T.cal) )^(r+x) * (beta+T.cal)^s / 
+      ((maxab+t.x)^s) 
+  }
+  else {
+    part2_times_F1_min_F2 = h2f1(r+s+x, param2, r+s+x+1, absab / (maxab+t.x)) * 
+      ( (alpha+T.cal)/(maxab+t.x) )^(r+x) * (beta+T.cal)^s / 
+      ((maxab+t.x)^s) -
+      h2f1(r+s+x, param2, r+s+x+1, absab / (maxab+T.cal)) * 
+      ( (alpha+T.cal)/(maxab+T.cal) )^(r+x) * (beta+T.cal)^s / 
+      ((maxab+t.x)^s)
+  }
+  return(part1 + part2 + log(1+(s/(r+s+x))*part2_times_F1_min_F2) )
 }
 
 pnbd.compress.cbs <- function(cbs, rounding = 3) {
